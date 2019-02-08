80b91b8fc219a41f702816fea22a7eef0031dc4d
Adouche Ali <adoucheali@yahoo.fr>
2019-02-08 15:23:07 +0100

    Last refacto after 4 hours of work

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,76 +1,95 @@
 package com.gildedrose;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class GildedRose {
 
 	public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 	public static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
 	public static final String AGED_BRIE = "Aged Brie";
+	public static final String CONJURED = "Conjured Mana Cake";
 
 	private static final int QUALITY_MIN = 0;
 	private static final int QUALITY_MAX = 50;
 
-	private Item[] items;
+	private List<Item> items;
 
-	public GildedRose() {}
-	
-	public GildedRose(Item[] items) {
-		this.items = items;
-	}
-	
-	public Item getItem(int position) {
-		return items[position];
+	public GildedRose() {
+		items = new ArrayList<>();
 	}
 
-	public void setItems(Item[] items) {
-		this.items = items;
+	public Item getItem(int index) {
+		return items.get(index);
+	}
+
+	public void addItem(Item item) {
+		items.add(item);
 	}
 
 	public void updateQuality() {
-		for (int i = 0; i < items.length; i++) {
+		for (Item item : items) {
+			updateQualityItem(item);
+		}
+	}
 
-			if(items[i].name.equals(SULFURAS)) {
-				continue;
+	private void updateQualityItem(Item item) {
+		if(SULFURAS.equals(item.getName())) {
+			return;
+		}
+		
+		item.setSellIn(item.getSellIn() - 1);
+		
+		if (BACKSTAGE.equals(item.getName())) {
+			
+			if(item.getSellIn() < 0) {
+				item.setQuality(0);
+				return;
 			}
 			
-			if (items[i].name.equals(BACKSTAGE)) {
-				if(items[i].quality < QUALITY_MAX) {
-					if (items[i].sellIn > 10) {
-						items[i].quality = items[i].quality + 1;
-					}else if (items[i].sellIn <= 10 && items[i].sellIn > 5) {
-						items[i].quality = items[i].quality + 2;
-					}else if (items[i].sellIn <= 5) {
-						items[i].quality = items[i].quality + 3;
-					}
-				}	
-			} else if (items[i].name.equals(AGED_BRIE)) {
-				if(items[i].quality < QUALITY_MAX) {
-					items[i].quality = items[i].quality + 1;
-				}
-			}else {
-				if (items[i].quality > QUALITY_MIN) {
-					items[i].quality = items[i].quality - 1;
-				}
+			increaseItemQuality(item);
+			
+			if (item.getSellIn() < 10) {
+				increaseItemQuality(item);
 			}
 			
-			items[i].sellIn = items[i].sellIn - 1;
+			if (item.getSellIn() < 5) {
+				increaseItemQuality(item);
+			}
 			
-			if (items[i].sellIn < 0) {
+		} else if (AGED_BRIE.equals(item.getName())) {
+			increaseItemQuality(item);
+			
+			if (item.getSellIn() < 0) {
+				increaseItemQuality(item);
+			}
+		}else {
+			decreaseQuality(item);
+			
+			if (CONJURED.equals(item.getName())) {
+				decreaseQuality(item);
+			}
+		}
+		
+	}
+
+	private void decreaseQuality(Item item) {
+		decreaseItemQuality(item);
+		
+		if (item.getSellIn() < 0) {
+			decreaseItemQuality(item);
+		}
+	}
+
+	private void increaseItemQuality(Item item) {
+		if(item.getQuality() < QUALITY_MAX) {
+			item.setQuality(item.getQuality() + 1);
+		}
+	}
 	
-				if (items[i].name.equals(BACKSTAGE)) {
-					
-					items[i].quality = 0;
-					
-				} else if (items[i].name.equals(AGED_BRIE)) {
-					
-					if (items[i].quality < QUALITY_MAX) {
-						items[i].quality = items[i].quality + 1;
-					}
-				} else {
-					if (items[i].quality > QUALITY_MIN) {
-						items[i].quality = items[i].quality - 1;
-					}
-				}
-			}
+	private void decreaseItemQuality(Item item) {
+		if(item.getQuality() > QUALITY_MIN) {
+			item.setQuality(item.getQuality() - 1);
 		}
 	}
 }
--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -1,21 +1,44 @@
 package com.gildedrose;
 
-public class Item {
+import java.io.Serializable;
 
-    public String name;
+public class Item implements Serializable {
+	private static final long serialVersionUID = 1L;
 
-    public int sellIn;
+	private String name;
 
-    public int quality;
+    private int sellIn;
+
+    private int quality;
 
     public Item(String name, int sellIn, int quality) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
     }
+    
+   public String getName() {
+		return name;
+	}
 
-   @Override
-   public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
+	public int getSellIn() {
+		return sellIn;
+	}
+
+	public int getQuality() {
+		return quality;
+	}
+
+	public void setSellIn(int sellIn) {
+		this.sellIn = sellIn;
+	}
+
+	public void setQuality(int quality) {
+		this.quality = quality;
+	}
+
+	@Override
+	public String toString() {
+        return String.join(", ",this.name, String.valueOf(this.sellIn), String.valueOf(this.quality));
     }
 }
