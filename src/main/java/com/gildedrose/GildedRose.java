909bcc327e97460c62c95fccacc8e680c94c587b
Adouche Ali <adoucheali@yahoo.fr>
2019-02-08 10:09:54 +0100

    after 2 hours of work

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,73 +1,73 @@
 package com.gildedrose;
 
 class GildedRose {
-	
+
 	public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 	public static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
 	public static final String AGED_BRIE = "Aged Brie";
-    
+
 	private static final int QUALITY_MIN = 0;
 	private static final int QUALITY_MAX = 50;
-	
+
 	Item[] items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
+	public GildedRose(Item[] items) {
+		this.items = items;
+	}
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-        	
-            if (!items[i].name.equals(AGED_BRIE) && !items[i].name.equals(BACKSTAGE)) {
-            	
-                if (items[i].quality > QUALITY_MIN) {
-                    if (!items[i].name.equals(SULFURAS)) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < QUALITY_MAX) {
-                	
-                    items[i].quality = items[i].quality + 1;
+	public void updateQuality() {
+		for (int i = 0; i < items.length; i++) {
 
-                    if (items[i].name.equals(BACKSTAGE)) {
-                    	
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < QUALITY_MAX) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+			if (!items[i].name.equals(AGED_BRIE) && !items[i].name.equals(BACKSTAGE) && !items[i].name.equals(SULFURAS)) {
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < QUALITY_MAX) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+				if (items[i].quality > QUALITY_MIN) {
+					items[i].quality = items[i].quality - 1;
+				}
 
-            if (!items[i].name.equals(SULFURAS)) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+			} else if (items[i].name.equals(BACKSTAGE)) {
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(AGED_BRIE)) {
-                    if (!items[i].name.equals(BACKSTAGE)) {
-                        if (items[i].quality > QUALITY_MIN) {
-                            if (!items[i].name.equals(SULFURAS)) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < QUALITY_MAX) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-        }
-    }
+					if (items[i].sellIn > 10) {
+						if (items[i].quality < QUALITY_MAX) {
+							items[i].quality = items[i].quality + 1;
+						}
+					}else if (items[i].sellIn <= 10 && items[i].sellIn > 5) {
+						if (items[i].quality < QUALITY_MAX) {
+							items[i].quality = items[i].quality + 2;
+						}
+					}else if (items[i].sellIn <= 5) {
+						if (items[i].quality < QUALITY_MAX) {
+							items[i].quality = items[i].quality + 3;
+						}
+					}
+					
+			} else if(items[i].quality < QUALITY_MAX) {
+					items[i].quality = items[i].quality + 1;
+			}
+
+			if (!items[i].name.equals(SULFURAS)) {
+				items[i].sellIn = items[i].sellIn - 1;
+			}
+			
+			if (items[i].sellIn < 0) {
+
+				if (items[i].name.equals(AGED_BRIE)) {
+					if (items[i].quality < QUALITY_MAX) {
+						items[i].quality = items[i].quality + 1;
+					}
+				}
+
+				if (items[i].name.equals(BACKSTAGE)) {
+					items[i].quality = 0;
+				}
+
+				if (!items[i].name.equals(AGED_BRIE) && !items[i].name.equals(BACKSTAGE) && !items[i].name.equals(SULFURAS)) {
+
+					if (items[i].quality > QUALITY_MIN) {
+						items[i].quality = items[i].quality - 1;
+					}
+					
+				}
+			}
+		}
+	}
 }
