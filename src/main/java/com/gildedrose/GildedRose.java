b56c8de82a56677e47e849e8b731b24ab1791291
Adouche Ali <adoucheali@yahoo.fr>
2019-02-08 11:01:26 +0100

    refacto after 3 hours of work

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,63 +9,66 @@ class GildedRose {
 	private static final int QUALITY_MIN = 0;
 	private static final int QUALITY_MAX = 50;
 
-	Item[] items;
+	private Item[] items;
 
+	public GildedRose() {}
+	
 	public GildedRose(Item[] items) {
+		this.items = items;
+	}
+	
+	public Item getItem(int position) {
+		return items[position];
+	}
+
+	public void setItems(Item[] items) {
 		this.items = items;
 	}
 
 	public void updateQuality() {
 		for (int i = 0; i < items.length; i++) {
 
-			if (!items[i].name.equals(AGED_BRIE) && !items[i].name.equals(BACKSTAGE) && !items[i].name.equals(SULFURAS)) {
-
+			if(items[i].name.equals(SULFURAS)) {
+				continue;
+			}
+			
+			if (items[i].name.equals(BACKSTAGE)) {
+				if(items[i].quality < QUALITY_MAX) {
+					if (items[i].sellIn > 10) {
+						items[i].quality = items[i].quality + 1;
+					}else if (items[i].sellIn <= 10 && items[i].sellIn > 5) {
+						items[i].quality = items[i].quality + 2;
+					}else if (items[i].sellIn <= 5) {
+						items[i].quality = items[i].quality + 3;
+					}
+				}	
+			} else if (items[i].name.equals(AGED_BRIE)) {
+				if(items[i].quality < QUALITY_MAX) {
+					items[i].quality = items[i].quality + 1;
+				}
+			}else {
 				if (items[i].quality > QUALITY_MIN) {
 					items[i].quality = items[i].quality - 1;
 				}
-
-			} else if (items[i].name.equals(BACKSTAGE)) {
-
-					if (items[i].sellIn > 10) {
-						if (items[i].quality < QUALITY_MAX) {
-							items[i].quality = items[i].quality + 1;
-						}
-					}else if (items[i].sellIn <= 10 && items[i].sellIn > 5) {
-						if (items[i].quality < QUALITY_MAX) {
-							items[i].quality = items[i].quality + 2;
-						}
-					}else if (items[i].sellIn <= 5) {
-						if (items[i].quality < QUALITY_MAX) {
-							items[i].quality = items[i].quality + 3;
-						}
-					}
-					
-			} else if(items[i].quality < QUALITY_MAX) {
-					items[i].quality = items[i].quality + 1;
-			}
-
-			if (!items[i].name.equals(SULFURAS)) {
-				items[i].sellIn = items[i].sellIn - 1;
 			}
 			
+			items[i].sellIn = items[i].sellIn - 1;
+			
 			if (items[i].sellIn < 0) {
-
-				if (items[i].name.equals(AGED_BRIE)) {
+	
+				if (items[i].name.equals(BACKSTAGE)) {
+					
+					items[i].quality = 0;
+					
+				} else if (items[i].name.equals(AGED_BRIE)) {
+					
 					if (items[i].quality < QUALITY_MAX) {
 						items[i].quality = items[i].quality + 1;
 					}
-				}
-
-				if (items[i].name.equals(BACKSTAGE)) {
-					items[i].quality = 0;
-				}
-
-				if (!items[i].name.equals(AGED_BRIE) && !items[i].name.equals(BACKSTAGE) && !items[i].name.equals(SULFURAS)) {
-
+				} else {
 					if (items[i].quality > QUALITY_MIN) {
 						items[i].quality = items[i].quality - 1;
 					}
-					
 				}
 			}
 		}
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,6 +2,7 @@ package com.gildedrose;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.Test;
 import static com.gildedrose.GildedRose.*;
 
@@ -10,139 +11,187 @@ public class GildedRoseTest {
 	private final static String OTHER_ITEM = "Elixir of the Mongoose";
 
 	private final static int SULFURAS_QUALITY = 80;
+	
+	private GildedRose app = new GildedRose();
+	private Item[] items;
+	
+	@After
+    public void after() throws Exception {
+		items = null;
+    }
 
 	@Test
-	public void should_neverAltersQuality_when_sulfuras() {
-		Item[] items = new Item[] { new Item(SULFURAS, 0, SULFURAS_QUALITY), new Item(SULFURAS, -1, SULFURAS_QUALITY) };
+	public void should_neverAltersQualityAndSellIn_when_sulfuras() {
+		int firstSellIn = 0;
+		int secondSellIn = -1;
+		
+		items = new Item[] { new Item(SULFURAS, firstSellIn, SULFURAS_QUALITY), new Item(SULFURAS, secondSellIn, SULFURAS_QUALITY) };
 
-		GildedRose app = new GildedRose(items);
+		app.setItems(items);
 		app.updateQuality();
-		assertEquals(SULFURAS, app.items[0].name);
-		assertEquals(SULFURAS_QUALITY, app.items[0].quality);
-		assertEquals(SULFURAS_QUALITY, app.items[1].quality);
+		
+		assertEquals(SULFURAS, app.getItem(0).name);
+		
+		assertEquals(SULFURAS_QUALITY, app.getItem(0).quality);
+		assertEquals(SULFURAS_QUALITY, app.getItem(1).quality);
+
+		assertEquals(firstSellIn, app.getItem(0).sellIn);
+		assertEquals(secondSellIn, app.getItem(1).sellIn);
 	}
 
 	@Test
 	public void should_increaseInQuality_when_agedBrieAndQualityLessThan50() {
 		int initialAgedBrieQuality = 0;
 		int expectedAgedBrieQuality = initialAgedBrieQuality + 1;
-		Item[] items = new Item[] { new Item(AGED_BRIE, 2, initialAgedBrieQuality) };
+		
+		int initialAgedBrieSellIn = 2;
+		int expectedAgedBrieSellIn = initialAgedBrieSellIn - 1;
+		
+		items = new Item[] { new Item(AGED_BRIE, initialAgedBrieSellIn, initialAgedBrieQuality) };
 
-		GildedRose app = new GildedRose(items);
+		app.setItems(items);
 		app.updateQuality();
-		assertEquals(AGED_BRIE, app.items[0].name);
-		assertEquals(expectedAgedBrieQuality, app.items[0].quality);
+		
+		assertEquals(AGED_BRIE, app.getItem(0).name);
+		assertEquals(expectedAgedBrieQuality, app.getItem(0).quality);
+		assertEquals(expectedAgedBrieSellIn, app.getItem(0).sellIn);
 	}
 
 	@Test
 	public void should_keepSameQuality_when_agedBrieAndQualityEquals50() {
 		int initialAgedBrieQuality = 50;
 		int expectedAgedBrieQuality = initialAgedBrieQuality;
-		Item[] items = new Item[] { new Item(AGED_BRIE, 2, initialAgedBrieQuality) };
+		
+		items = new Item[] { new Item(AGED_BRIE, 2, initialAgedBrieQuality) };
 
-		GildedRose app = new GildedRose(items);
+		app.setItems(items);
 		app.updateQuality();
-		assertEquals(AGED_BRIE, app.items[0].name);
-		assertEquals(expectedAgedBrieQuality, app.items[0].quality);
+		
+		assertEquals(AGED_BRIE, app.getItem(0).name);
+		assertEquals(expectedAgedBrieQuality, app.getItem(0).quality);
 	}
 
 	@Test
 	public void should_increaseInQualityBy2_when_agedBrieAndSellInPassed() {
 		int initialAgedBrieQuality = 2;
 		int expectedAgedBrieQuality = initialAgedBrieQuality + 2;
-		Item[] items = new Item[] { new Item(AGED_BRIE, -1, initialAgedBrieQuality) };
+		
+		items = new Item[] { new Item(AGED_BRIE, -1, initialAgedBrieQuality) };
 
-		GildedRose app = new GildedRose(items);
+		app.setItems(items);
 		app.updateQuality();
-		assertEquals(AGED_BRIE, app.items[0].name);
-		assertEquals(expectedAgedBrieQuality, app.items[0].quality);
+		
+		assertEquals(AGED_BRIE, app.getItem(0).name);
+		assertEquals(expectedAgedBrieQuality, app.getItem(0).quality);
 	}
 
 	@Test
 	public void should_increaseInQuality_when_backstageAndQualityLessThan50() {
 		int initialBackstageQuality = 0;
 		int expectedBackstageQuality = initialBackstageQuality + 1;
-		Item[] items = new Item[] { new Item(BACKSTAGE, 20, initialBackstageQuality) };
+		
+		int initialBackstageSellIn = 20;
+		int expectedBackstageSellIn = initialBackstageSellIn - 1;
+		
+		items = new Item[] { new Item(BACKSTAGE, initialBackstageSellIn, initialBackstageQuality) };
 
-		GildedRose app = new GildedRose(items);
+		app.setItems(items);
 		app.updateQuality();
-		assertEquals(BACKSTAGE, app.items[0].name);
-		assertEquals(expectedBackstageQuality, app.items[0].quality);
+		
+		assertEquals(BACKSTAGE, app.getItem(0).name);
+		assertEquals(expectedBackstageQuality, app.getItem(0).quality);
+		assertEquals(expectedBackstageSellIn, app.getItem(0).sellIn);
 	}
 
 	@Test
 	public void should_keepSameQuality_when_backstageAndQualityEquals50() {
 		int initialBackstageQuality = 50;
 		int expectedBackstageQuality = initialBackstageQuality;
-		Item[] items = new Item[] { new Item(BACKSTAGE, 20, initialBackstageQuality) };
+		
+		items = new Item[] { new Item(BACKSTAGE, 20, initialBackstageQuality) };
 
-		GildedRose app = new GildedRose(items);
+		app.setItems(items);
 		app.updateQuality();
-		assertEquals(BACKSTAGE, app.items[0].name);
-		assertEquals(expectedBackstageQuality, app.items[0].quality);
+		
+		assertEquals(BACKSTAGE, app.getItem(0).name);
+		assertEquals(expectedBackstageQuality, app.getItem(0).quality);
 	}
 
 	@Test
 	public void should_increaseInQualityBy2_when_backstageAndSellInBetween10And6() {
 		int initialBackstageQuality = 16;
 		int expectedBackstageQuality = initialBackstageQuality + 2;
-		Item[] items = new Item[] { new Item(BACKSTAGE, 10, initialBackstageQuality),
+		
+		items = new Item[] { new Item(BACKSTAGE, 10, initialBackstageQuality),
 				new Item(BACKSTAGE, 6, initialBackstageQuality) };
 
-		GildedRose app = new GildedRose(items);
+		app.setItems(items);
 		app.updateQuality();
-		assertEquals(BACKSTAGE, app.items[0].name);
-		assertEquals(expectedBackstageQuality, app.items[0].quality);
-		assertEquals(expectedBackstageQuality, app.items[1].quality);
+		
+		assertEquals(BACKSTAGE, app.getItem(0).name);
+		assertEquals(expectedBackstageQuality, app.getItem(0).quality);
+		assertEquals(expectedBackstageQuality, app.getItem(1).quality);
 	}
 
 	@Test
 	public void should_increaseInQualityBy3_when_backstageAndSellInBetween5And1() {
 		int initialBackstageQuality = 15;
 		int expectedBackstageQuality = initialBackstageQuality + 3;
-		Item[] items = new Item[] { new Item(BACKSTAGE, 5, initialBackstageQuality),
+		
+		items = new Item[] { new Item(BACKSTAGE, 5, initialBackstageQuality),
 				new Item(BACKSTAGE, 1, initialBackstageQuality) };
 
-		GildedRose app = new GildedRose(items);
+		app.setItems(items);
 		app.updateQuality();
-		assertEquals(BACKSTAGE, app.items[0].name);
-		assertEquals(expectedBackstageQuality, app.items[0].quality);
-		assertEquals(expectedBackstageQuality, app.items[1].quality);
+		
+		assertEquals(BACKSTAGE, app.getItem(0).name);
+		assertEquals(expectedBackstageQuality, app.getItem(0).quality);
+		assertEquals(expectedBackstageQuality, app.getItem(1).quality);
 	}
 
 	@Test
 	public void should_dropQualityTo0_when_backstageAndAfterTheCancert() {
 		int initialBackstageQuality = 50;
 		int expectedBackstageQuality = 0;
-		Item[] items = new Item[] { new Item(BACKSTAGE, -1, initialBackstageQuality) };
+		
+		items = new Item[] { new Item(BACKSTAGE, -1, initialBackstageQuality) };
 
-		GildedRose app = new GildedRose(items);
+		app.setItems(items);
 		app.updateQuality();
-		assertEquals(BACKSTAGE, app.items[0].name);
-		assertEquals(expectedBackstageQuality, app.items[0].quality);
+		
+		assertEquals(BACKSTAGE, app.getItem(0).name);
+		assertEquals(expectedBackstageQuality, app.getItem(0).quality);
 	}
 
 	@Test
 	public void should_decreaseInQuality_when_otherItem() {
-		int initialBackstageQuality = 10;
-		int expectedBackstageQuality = initialBackstageQuality - 1;
-		Item[] items = new Item[] { new Item(OTHER_ITEM, 10, initialBackstageQuality) };
+		int initialOtherItemQuality = 10;
+		int expectedOtherItemQuality = initialOtherItemQuality - 1;
+		
+		int initialOtherItemSellIn = 10;
+		int expectedOtherItemSellIn = initialOtherItemSellIn - 1;
+		
+		items = new Item[] { new Item(OTHER_ITEM, initialOtherItemSellIn, initialOtherItemQuality) };
 
-		GildedRose app = new GildedRose(items);
+		app.setItems(items);
 		app.updateQuality();
-		assertEquals(OTHER_ITEM, app.items[0].name);
-		assertEquals(expectedBackstageQuality, app.items[0].quality);
+		
+		assertEquals(OTHER_ITEM, app.getItem(0).name);
+		assertEquals(expectedOtherItemQuality, app.getItem(0).quality);
+		assertEquals(expectedOtherItemSellIn, app.getItem(0).sellIn);
 	}
 
 	@Test
 	public void should_decreaseInQuality_when_otherItemAndSellInPassed() {
-		int initialBackstageQuality = 10;
-		int expectedBackstageQuality = initialBackstageQuality - 2;
-		Item[] items = new Item[] { new Item(OTHER_ITEM, -1, initialBackstageQuality) };
+		int initialOtherItemQuality = 10;
+		int expectedOtherItemQuality = initialOtherItemQuality - 2;
+		
+		items = new Item[] { new Item(OTHER_ITEM, -1, initialOtherItemQuality) };
 
-		GildedRose app = new GildedRose(items);
+		app.setItems(items);
 		app.updateQuality();
-		assertEquals(OTHER_ITEM, app.items[0].name);
-		assertEquals(expectedBackstageQuality, app.items[0].quality);
+		
+		assertEquals(OTHER_ITEM, app.getItem(0).name);
+		assertEquals(expectedOtherItemQuality, app.getItem(0).quality);
 	}
 }
